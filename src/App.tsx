aa9b83e2ca0e8ce7cfddbba4deea506d632b36aa
Max Prais <maxprais@gmail.com>
2024-10-31 12:56:13 +0000

    add tax calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import SignUp from "./components/SignUp";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { TaxCalculator } from "./components/TaxCalculator";
 
 function App() {
-  return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route
-          path="/sign-up/partner/:partnerName/code/:partnerCode"
-          Component={() => <SignUp />}
-        />
-
-        <Route path="/sign-up/:inviteCode" Component={() => <SignUp />} />
-
-        <Route
-          path="/success"
-          Component={() => (
-            <div className="container">
-              Thanks, you will receive an email shortly to complete your signup.
-            </div>
-          )}
-        />
-
-        <Route
-          path="/"
-          Component={() => (
-            <div className="container">
-              <h1>Onsi Frontend Interview</h1>
-              <Link to="/sign-up/example_code">
-                Example 'Invite Code' sign up
-              </Link>
-              <Link to="/sign-up/partner/food_and_co/code/example_code">
-                Example 'Partner Code' sign up
-              </Link>
-            </div>
-          )}
-        />
-
-        <Route path="*" Component={() => <div>404, page not found!</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <TaxCalculator />;
 }
 
 export default App;
---
+++ src/components/TaxCalculator.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+
+type Pair<A, B> = {
+  first: A;
+  second: B;
+};
+
+export const TaxCalculator = () => {
+  const [percent, setPercent] = useState(20);
+
+  const [currency, setCurrency] = useState("GBP");
+  const [amount, setAmount] = useState(0);
+
+  const [pairs, setPairs] = useState<Pair<number, string>[]>([]);
+
+  const netAmount = (
+    percent: number,
+    rest: Pair<number, string>[]
+  ): Pair<number, string> => {
+    if (!rest.length) {
+      return { first: 0, second: "GBP" };
+    }
+
+    let [first, ...pairs] = rest;
+
+    let total: Pair<number, string> = first;
+
+    for (let next of pairs) {
+      if (next.second != total.second) {
+        throw new Error();
+      }
+    }
+
+    for (let next of pairs) {
+      total = {
+        first: total.first + next.first,
+        second: next.second,
+      };
+    }
+
+    let tax = total.first * (percent / 100);
+
+    return {
+      first: total.first - tax,
+      second: total.second,
+    };
+  };
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          setPairs([...pairs, { first: amount, second: currency }]);
+        }}
+      >
+        <input
+          type="text"
+          name="percent"
+          value={percent}
+          onChange={(e) => setPercent(Number(e.target?.value))}
+        />
+
+        <select
+          name="currency"
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+        >
+          <option value="GBP">GBP</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+        </select>
+
+        <input
+          name="amount"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        {pairs.map((pair, index) => (
+          <p key={`${pair.first}${index}`}>
+            {pair.second} {pair.first}
+          </p>
+        ))}
+      </ul>
+
+      <p>
+        Total:{" "}
+        {`${netAmount(percent, pairs).first} ${
+          netAmount(percent, pairs).second
+        }`}
+      </p>
+    </div>
+  );
+};
